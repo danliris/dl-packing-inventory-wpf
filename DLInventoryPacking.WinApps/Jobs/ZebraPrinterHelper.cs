b0da8f3fa98fbe9a795a940ea18e2f6449e354de
Leslie Aula <leslie.aula@moonlay.com>
2020-05-05 11:23:12 +0700

    Implement ZPL

---
+++ DLInventoryPacking.WinApps/Jobs/ZebraPrinterHelper.cs
@@ -0,0 +1,258 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Zebra.Sdk.Comm;
+using Zebra.Sdk.Printer;
+using Zebra.Sdk.Printer.Discovery;
+
+namespace DLInventoryPacking.WinApps.Jobs
+{
+    public static class ZebraPrinterHelper
+    {
+        public static async Task Print(string zpl)
+        {
+
+            await Task.Run(() =>
+            {
+                DiscoveredPrinter discoveredPrinter = GetUSBPrinter();
+
+                ZebraPrinter printer = PrintHelper.Connect(discoveredPrinter, PrinterLanguage.ZPL);
+                PrintHelper.SetPageLanguage(printer);
+                if (PrintHelper.CheckStatus(printer))
+                {
+                    PrintHelper.Print(printer, zpl);
+                    if (PrintHelper.CheckStatusAfter(printer))
+                    {
+                        Console.WriteLine($"Label Printed");
+                    }
+                }
+                printer = PrintHelper.Disconnect(printer);
+                Console.WriteLine("Done Printing");
+            });
+        }
+
+        public static DiscoveredPrinter GetUSBPrinter()
+        {
+            DiscoveredPrinter discoveredPrinter = null;
+            try
+            {
+                foreach (DiscoveredUsbPrinter usbPrinter in UsbDiscoverer.GetZebraUsbPrinters())
+                {
+                    discoveredPrinter = usbPrinter;
+                    Console.WriteLine(usbPrinter);
+                }
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Error discovering local printers: {e.Message}");
+            }
+
+            Console.WriteLine("Done discovering local printers.");
+            return discoveredPrinter;
+        }
+    }
+
+    public class PrintHelper
+    {
+        public static bool CheckStatusAfter(ZebraPrinter printer)
+        {
+            PrinterStatus printerStatus = null;
+            try
+            {
+                VerifyConnection(printer);
+                printerStatus = printer.GetCurrentStatus();
+                while ((printerStatus.numberOfFormatsInReceiveBuffer > 0) && (printerStatus.isReadyToPrint))
+                {
+                    Thread.Sleep(500);
+                    printerStatus = printer.GetCurrentStatus();
+                }
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Error getting status from printer: {e.Message}");
+            }
+
+            if (printerStatus.isReadyToPrint)
+            {
+                Console.WriteLine($"Ready To Print");
+                return true;
+            }
+            else if (printerStatus.isPaused)
+            {
+                Console.WriteLine($"Cannot Print because the printer is paused.");
+            }
+            else if (printerStatus.isHeadOpen)
+            {
+                Console.WriteLine($"Cannot Print because the printer head is open.");
+            }
+            else if (printerStatus.isPaperOut)
+            {
+                Console.WriteLine($"Cannot Print because the paper is out.");
+            }
+            else
+            {
+                Console.WriteLine($"Cannot Print.");
+            }
+            return false;
+        }
+        public static bool Print(ZebraPrinter printer, string printstring)
+        {
+            bool sent = false;
+            try
+            {
+                VerifyConnection(printer);
+                printer.Connection.Write(Encoding.UTF8.GetBytes(printstring));
+                sent = true;
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Unable to write to printer: {e.Message}");
+            }
+            return sent;
+        }
+        public static bool SetPageLanguage(ZebraPrinter printer)
+        {
+            bool set = false;
+            string setLang = "zpl";
+            if (PrinterLanguage.ZPL != printer.PrinterControlLanguage)
+            {
+                setLang = "line_print";
+            }
+
+            try
+            {
+                VerifyConnection(printer);
+                SGD.SET("device.languages", setLang, printer.Connection);
+                string getLang = SGD.GET("device.languages", printer.Connection);
+                if (getLang.Contains(setLang))
+                {
+                    set = true;
+                }
+                else
+                {
+                    Console.WriteLine($"This is not a {setLang} printer.");
+                }
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Unable to set print language: {e.Message}");
+            }
+            return set;
+        }
+        public static bool CheckStatus(ZebraPrinter printer)
+        {
+            PrinterStatus printerStatus = null;
+            try
+            {
+                VerifyConnection(printer);
+                printerStatus = printer.GetCurrentStatus();
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Error getting status from printer: {e.Message}");
+            }
+
+            if (null == printerStatus)
+            {
+                Console.WriteLine($"Unable to get status.");
+            }
+            else if (printerStatus.isReadyToPrint)
+            {
+                Console.WriteLine($"Ready To Print");
+                return true;
+            }
+            else if (printerStatus.isPaused)
+            {
+                Console.WriteLine($"Cannot Print because the printer is paused.");
+            }
+            else if (printerStatus.isHeadOpen)
+            {
+                Console.WriteLine($"Cannot Print because the printer head is open.");
+            }
+            else if (printerStatus.isPaperOut)
+            {
+                Console.WriteLine($"Cannot Print because the paper is out.");
+            }
+            else
+            {
+                Console.WriteLine($"Cannot Print.");
+            }
+            return false;
+        }
+        public static bool VerifyConnection(ZebraPrinter printer)
+        {
+            bool ok = false;
+            try
+            {
+                if (!printer.Connection.Connected)
+                {
+                    printer.Connection.Open();
+                    if (printer.Connection.Connected)
+                        ok = true;
+                }
+                else ok = true;
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Unable to connect to printer: {e.Message}");
+            }
+            return ok;
+        }
+        public static ZebraPrinter Connect(Connection connection, PrinterLanguage language)
+        {
+            ZebraPrinter printer = null;
+            try
+            {
+                connection.Open();
+                if (connection.Connected)
+                {
+                    printer = ZebraPrinterFactory.GetInstance(language, connection);
+                    Console.WriteLine($"Printer Connected");
+                }
+                else Console.WriteLine($"Printer Not Connected!");
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Error connecting to printer: {e.Message}");
+            }
+            return printer;
+        }
+        public static ZebraPrinter Connect(DiscoveredPrinter discoveredPrinter, PrinterLanguage language)
+        {
+            ZebraPrinter printer = null;
+            try
+            {
+                Connection connection = discoveredPrinter.GetConnection();
+                printer = ZebraPrinterFactory.GetInstance(language, connection);
+                printer.Connection.Open();
+                if (printer.Connection.Connected)
+                    Console.WriteLine($"Printer Connected");
+                else Console.WriteLine($"Printer Not Connected!");
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Error connecting to printer: {e.Message}");
+            }
+            return printer;
+        }
+        public static ZebraPrinter Disconnect(ZebraPrinter printer)
+        {
+            try
+            {
+                printer.Connection.Close();
+                Console.WriteLine($"Printer Disconnected");
+            }
+            catch (ConnectionException e)
+            {
+                Console.WriteLine($"Error disconnecting from printer: {e.Message}");
+            }
+            return printer;
+        }
+    }
+}
--- DLInventoryPacking.WinApps/Pages/YarnBarcodePage.xaml.cs
+++ DLInventoryPacking.WinApps/Pages/YarnBarcodePage.xaml.cs
@@ -100,13 +100,16 @@ namespace DLInventoryPacking.WinApps.Pages
 
         }
 
-        private void PrintButton_Click(object sender, RoutedEventArgs e)
+        private async void PrintButton_Click(object sender, RoutedEventArgs e)
         {
-            if (BarcodeListView.Items.Count > 0)
-            {
-                var printBarcodeJob = new BarcodePrintJob();
-                printBarcodeJob.PrintBartenderJob(_barcodes);
-            }
+            //if (BarcodeListView.Items.Count > 0)
+            //{
+            //    var printBarcodeJob = new BarcodePrintJob();
+            //    printBarcodeJob.PrintBartenderJob(_barcodes);
+            //}
+            var zpl1 = "CT~~CD,~CC^~CT~^XA~TA000~JSN^LT0^MNM,0^MTT^PON^PMN^LH0,0^JMA^PR3,3~SD30^JUS^LRN^CI27^PA0,1,1,0^XZ^XA^MMT^PW615^LL328^LS0^FWR^FO415,70^BQ,2,8^FDQA,200100001PALET^FS^FO380,80^AD,12,12^FD200100001PALET^FS^FO100,25^BY2^BC,75,Y,N,N^FD200100001^FS^FO306,65^AD,12,12^FDPC25 LOT 1^FS^FO280,65^AD,11,11^FD2 BALE^FS^FO0,25^AD,10,10^FD28/04/2020^FS^PQ1,0,1,Y^XZ";
+            var zpl2 = "CT~~CD,~CC^~CT~^XA~TA000~JSN^LT0^MNM,0^MTT^PON^PMN^LH0,0^JMA^PR3,3~SD30^JUS^LRN^CI27^PA0,1,1,0^XZ^XA^MMT^PW615^LL328^LS0^FWR^FO415,70^BQ,2,8^FDQA,FD200200002PCS^FS^FO380,80^AD,12,12^FD200200002PCS^FS^FO100,25^BY2^BC,75,Y,N,N^FD200200002^FS^FO306,65^AD,12,12^FDPC25 LOT 1^FS^FO280,65^AD,11,11^FD2 BALE^FS^FO0,25^AD,10,10^FD28/04/2020^FS^PQ1,0,1,Y^XZ";
+            await ZebraPrinterHelper.Print(zpl1 + zpl2 + zpl1);
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
