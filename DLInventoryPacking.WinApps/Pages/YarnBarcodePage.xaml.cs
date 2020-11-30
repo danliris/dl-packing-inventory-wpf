8bb9e79815c58ba4e901d0c0f1f391f835de1ca8
Leslie Aula <leslie.aula@moonlay.com>
2020-11-30 15:08:28 +0700

    add cache

--- DLInventoryPacking.WinApps/Pages/YarnBarcodePage.xaml.cs
+++ DLInventoryPacking.WinApps/Pages/YarnBarcodePage.xaml.cs
@@ -2,6 +2,8 @@
 using DLInventoryPacking.WinApps.Services;
 using DLInventoryPacking.WinApps.Services.ResponseModel;
 using DLInventoryPacking.WinApps.ViewModels;
+using Newtonsoft.Json;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,7 @@ namespace DLInventoryPacking.WinApps.Pages
     public partial class YarnBarcodePage : Page
     {
         private List<BarcodeInfo> _barcodes;
+        private readonly IDatabase _cache;
 
         public YarnBarcodePage()
         {
@@ -25,6 +28,7 @@ namespace DLInventoryPacking.WinApps.Pages
             _barcodes = new List<BarcodeInfo>();
             pb.Visibility = Visibility.Hidden;
             DeleteButton.IsEnabled = false;
+            _cache = CacheService.Connection.GetDatabase();
             //PrintButton.IsEnabled = false;
         }
 
@@ -84,10 +88,32 @@ namespace DLInventoryPacking.WinApps.Pages
                 //    );
                 var barcodeList = await PackingInventoryService.GetBarcodeInfoByOrderNo(OrderNo.Text, IsReprint.IsChecked.GetValueOrDefault());
 
+
+
+
                 _barcodes = new List<BarcodeInfo>();
                 BarcodeListView.Items.Clear();
                 foreach (var barcode in barcodeList)
                 {
+                    var printedListJson = _cache.StringGet(barcode.productionOrder.no);
+                    var printedList = new List<string>();
+
+                    if (!string.IsNullOrWhiteSpace(printedListJson))
+                    {
+                        printedList = JsonConvert.DeserializeObject<List<string>>(printedListJson, new JsonSerializerSettings()
+                        {
+                            MissingMemberHandling = MissingMemberHandling.Ignore
+                        });
+                    }
+
+                    if (printedList != null && printedList.Count > 0)
+                    {
+                        if (IsReprint.IsChecked.GetValueOrDefault())
+                            barcode.productPackingCodes = barcode.productPackingCodes.Where(element => printedList.Contains(element)).ToList();
+                        else
+                            barcode.productPackingCodes = barcode.productPackingCodes.Where(element => !printedList.Contains(element)).ToList();
+                    }
+
                     foreach (var packingCode in barcode.productPackingCodes)
                     {
                         var barcodeInfo = new BarcodeInfo()
@@ -156,8 +182,27 @@ namespace DLInventoryPacking.WinApps.Pages
         {
             var zplString = "";
             var barcodes = BarcodeListView.SelectedItems.Cast<BarcodeInfo>().ToArray();
+
             foreach (var barcode in barcodes)
             {
+                var printedListJson = _cache.StringGet(barcode.OrderNo);
+                var printedList = new List<string>();
+
+                if (!string.IsNullOrWhiteSpace(printedListJson))
+                {
+                    printedList = JsonConvert.DeserializeObject<List<string>>(printedListJson, new JsonSerializerSettings()
+                    {
+                        MissingMemberHandling = MissingMemberHandling.Ignore
+                    });
+
+                    if (printedList == null)
+                        printedList = new List<string>();
+                }
+
+                printedList.Add(barcode.PackingCode);
+                printedListJson = JsonConvert.SerializeObject(printedList);
+                _cache.StringSet(OrderNo.Text, printedListJson);
+
                 zplString += $"^XA^MMT^PW382^LL0635^LS0^CFB,15,7^FO20,250^FDAnyaman^FS^FO150,250^FD{barcode.MaterialName}^FS^FO20,275^FDKonstruksi^FS^FO150,275^FD{barcode.MaterialConstructionName}^FS^FO250,275^FD{barcode.YarnMaterialName}^FS^FO20,300^FDPanjang^FS^FO150,300^FD{barcode.PackingLength}^FS^FO250,300^FD{barcode.UOMSKU}^FS^FO20,325^FDMotif/Warna^FS^FO150,325^FD{barcode.Color}^FS^FO20,550^FD{DateTime.Now}^FS^FT131,169^BQN,2,5^FH\\^FDLA,{barcode.PackingCode}^FS^FT122,182^A0N,16,21^FB125,1,0,C^FH\\^FD{barcode.PackingCode}^FS^PQ1,0,1,Y^XZ";
             }
             //var zpl2 = "CT~~CD,~CC^~CT~^XA~TA000~JSN^LT0^MNM,0^MTT^PON^PMN^LH0,0^JMA^PR3,3~SD30^JUS^LRN^CI27^PA0,1,1,0^XZ^XA^MMT^PW615^LL328^LS0^FWR^FO415,70^BQ,2,8^FDQA,FD200200002PCS^FS^FO380,80^AD,12,12^FD200200002PCS^FS^FO100,25^BY2^BC,75,Y,N,N^FD200200002^FS^FO306,65^AD,12,12^FDPC25 LOT 1^FS^FO280,65^AD,11,11^FD2 BALE^FS^FO0,25^AD,10,10^FD28/04/2020^FS^PQ1,0,1,Y^XZ";
---
+++ DLInventoryPacking.WinApps/Services/CacheService.cs
@@ -0,0 +1,26 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLInventoryPacking.WinApps.Services
+{
+    public static class CacheService
+    {
+        private static readonly Lazy<ConnectionMultiplexer> _lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            var cacheConnection = "dl-cache.redis.cache.windows.net:6380,password=fspdi3JK1OY3uFXzl1XyB3igmqSKwnSq4IkHs3pKVe0=,ssl=True,abortConnect=False";
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return _lazyConnection.Value;
+            }
+        }
+    }
+}
